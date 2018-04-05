9f9d42a978d1b70cfc616fa83a2fb357559a8687
tgwt <megatoaster94@gmail.com>
2018-04-05 10:46:01 +0300

    delete extra files

--- source/5.7/android_armv7/lib/cmake/Qt5Gamepad/Qt5Gamepad_QAndroidGamepadBackendPlugin.cmake
+++
@@ -1,6 +0,0 @@
-
-add_library(Qt5::QAndroidGamepadBackendPlugin MODULE IMPORTED)
-
-_populate_Gamepad_plugin_properties(QAndroidGamepadBackendPlugin RELEASE "gamepads/libandroidgamepad.so")
-
-list(APPEND Qt5Gamepad_PLUGINS Qt5::QAndroidGamepadBackendPlugin)
--- source/5.7/android_armv7/lib/cmake/Qt5Network/Qt5Network_QAndroidBearerEnginePlugin.cmake
+++
@@ -1,6 +0,0 @@
-
-add_library(Qt5::QAndroidBearerEnginePlugin MODULE IMPORTED)
-
-_populate_Network_plugin_properties(QAndroidBearerEnginePlugin RELEASE "bearer/libqandroidbearer.so")
-
-list(APPEND Qt5Network_PLUGINS Qt5::QAndroidBearerEnginePlugin)
